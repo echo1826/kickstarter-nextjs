c366fabbd2de565876ef9d3ba21d5d49202a0952
Ethan <ethan.cho1826@gmail.com>
2022-08-09 16:43:44 -0500

    server side data fetching with nextjs in the homepage component

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,16 +2,11 @@ import Head from "next/head";
 import factory from "../ethereum/factory";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-    const [campaignList, setCampaignList] = useState([]);
-
-    useEffect(() => {
-        async function fetchData() {
-            const campaigns = await factory.methods.getCampaigns().call();
-            setCampaignList(campaigns);
-        }
-        fetchData();
-    }, []);
+export default function Home({ campaigns }) {
+    Home.getInitialProps = async () => {
+        const campaigns = await factory.methods.getCampaigns().call();
+        return { campaigns };
+    };
 
     return (
         <div>
@@ -23,7 +18,7 @@ export default function Home() {
                 />
             </Head>
             <h1>This is the campaign list</h1>
-            
+            <div>{campaigns}</div>
         </div>
     );
 }
