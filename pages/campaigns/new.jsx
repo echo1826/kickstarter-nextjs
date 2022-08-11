5b08961d18c406435308a15e896a37c09119c232
Ethan <ethan.cho1826@gmail.com>
2022-08-11 17:05:20 -0500

    setup some basic error handling

--- pages/campaigns/new.jsx
+++ pages/campaigns/new.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Container, Button, Input } from "semantic-ui-react";
+import { Form, Container, Button, Input, Label } from "semantic-ui-react";
 import Layout from "../../components/layout";
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
@@ -8,17 +8,26 @@ import "semantic-ui-css/semantic.min.css";
 // nested route: /campaigns/new
 function NewCampaign() {
     const [input, setInput] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     function handleInputChange(event) {
         event.preventDefault();
         setInput(event.target.value);
     }
 
     async function handleSubmit(event) {
-        event.prevenDefault();
+        event.preventDefault();
+        if(!input) {
+            setErrorMessage("Please set a minimum contribution amount for the new campaign");
+            return;
+        }
         const accounts = await web3.eth.getAccounts();
         await factory.methods.createCampaign(input).send({
             from: accounts[0]
         });
+        if(errorMessage) {
+            setErrorMessage("");
+        }
+        setInput("");
     }
 
     return (
@@ -37,6 +46,7 @@ function NewCampaign() {
                                 value={input}
                                 onChange={handleInputChange}
                             />
+                            {errorMessage && <Label basic color='red' pointing>{errorMessage}</Label>}
                         </Form.Field>
                         <Button primary>Create!</Button>
                     </Form>
