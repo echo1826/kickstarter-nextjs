9eea24d976ab3eff7511a537874df69b37d7e167
Ethan <ethan.cho1826@gmail.com>
2022-08-13 00:02:32 -0500

    added routing to header, added layout to the campaign details component

--- components/header.jsx
+++ components/header.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import React from "react";
+import Link from "next/link";
+import { Menu } from "semantic-ui-react";
 
 function Header() {
     return (
-        <Menu style={{marginTop: '1%'}}>
+        <Menu style={{ marginTop: "1%" }}>
             <Menu.Item>
-                CrowdCoin
+                <Link href="/">CrowdCoin</Link>
             </Menu.Item>
-            <Menu.Menu position='right'>
-                <Menu.Item>
-                Campaigns
-                </Menu.Item>
-                <Menu.Item>+</Menu.Item>
+            <Menu.Menu position="right">
+                <Link href="/">
+                    <Menu.Item>Campaigns</Menu.Item>
+                </Link>
+                <Link href="/campaigns/new">
+                    <Menu.Item>+</Menu.Item>
+                </Link>
             </Menu.Menu>
         </Menu>
-    )
+    );
 }
 
 export default Header;
--- pages/campaigns/[campaignAddress]/index.jsx
+++ pages/campaigns/[campaignAddress]/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import {useRouter} from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
+import Layout from "../../../components/layout";
+import "semantic-ui-css/semantic.min.css";
 
 function CampaignDetails() {
     const router = useRouter();
-    const address = router.query
+    const address = router.query;
     console.log(address);
 
     return (
-        <h1>Campaign Details for a specific campaign</h1>
-    )
+        <>
+            <Layout>
+                <h1>Campaign Details for a specific campaign</h1>
+            </Layout>
+        </>
+    );
 }
 
 export default CampaignDetails;
