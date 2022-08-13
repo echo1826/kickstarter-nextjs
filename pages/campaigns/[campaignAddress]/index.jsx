2c7e5da9250d3430ea557f1efd38573995175668
Ethan <ethan.cho1826@gmail.com>
2022-08-13 00:50:52 -0500

    added hdwallet-provider, started getInitialProps on campaign details component

--- pages/campaigns/[campaignAddress]/index.jsx
+++ pages/campaigns/[campaignAddress]/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../../components/layout";
 import "semantic-ui-css/semantic.min.css";
+import web3 from '../../../ethereum/web3';
+import factory from '../../../ethereum/factory';
 
-function CampaignDetails() {
+function CampaignDetails({balance}) {
     const router = useRouter();
-    const address = router.query;
-    console.log(address);
+    // const {address} = router.query;
+    console.log(balance);
 
     return (
         <>
@@ -17,4 +19,7 @@ function CampaignDetails() {
     );
 }
 
+CampaignDetails.getInitialProps = async ({query}) => {
+    
+}
 export default CampaignDetails;
