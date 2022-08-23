22e572df8fb15cb8e69f0db946630151da9bde1a
Ethan Cho <ethan.cho1826@gmail.com>
2022-08-22 19:44:09 -0500

    added state for input handling and loading flag

--- components/contributeForm.jsx
+++ components/contributeForm.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Container, Button, Input, Message } from "semantic-ui-react";
 
 function ContributeForm() {
+    const [input, setInput] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("submitted");
+        setLoading(true);
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Field>
                 <label>Amount to Contribute</label>
-                <Input 
-                label="ether"
-                labelPosition="right" 
+                <Input
+                    label="ether"
+                    labelPosition="right"
+                    value={input}
+                    onChange={(event) => {
+                        event.preventDefault();
+                        setInput(event.target.value);
+                    }}
                 />
             </Form.Field>
             <Button primary>Contribute</Button>
