02ea8c9460574446c570eb87d0fd29286118e677
Ethan <ethan.cho1826@gmail.com>
2022-08-31 14:38:09 -0500

    new request form page, linked from request list page

--- pages/campaigns/[campaignAddress]/requests/index.jsx
+++ pages/campaigns/[campaignAddress]/requests/index.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
+import Layout from "../../../../components/layout";
+import { Button } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-function RequestList() {
-    return <></>;
+function RequestList({campaignAddress}) {
+    
+    return (
+        <>
+            <Layout>
+                <Link href={`/campaigns/${campaignAddress}/requests/new`}>
+                    <Button primary>Add Request</Button>
+                </Link>
+            </Layout>
+        </>
+    );
 }
+
+RequestList.getInitialProps = async (props) => {
+    const { campaignAddress } = props.query;
+    return { campaignAddress };
+};
 
 export default RequestList;
--- pages/campaigns/[campaignAddress]/requests/new.jsx
+++ pages/campaigns/[campaignAddress]/requests/new.jsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import campaignConstructor from "../../../../ethereum/campaign";
+import web3 from "../../../../ethereum/web3";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Form, Button, Message, Input } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-function RequestForm() {
+function RequestForm({ campaignAddress }) {
+    const [inputs, setInputs] = useState({
+        description: "",
+        amount: "",
+        recipient: "",
+    });
 
-    return <></>;
+    return (
+        <>
+            <Form>
+                <Form.Field>
+                    <label htmlFor="description">Description</label>
+                    <Input
+                        id="description"
+                        value={inputs.description}
+                        onChange={(event) =>
+                            setInputs({
+                                ...inputs,
+                                description: event.target.value,
+                            })
+                        }
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="amount">Amount in Ether</label>
+                    <Input
+                        id="amount"
+                        value={inputs.amount}
+                        onChange={(event) =>
+                            setInputs({ ...inputs, amount: event.target.value })
+                        }
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="recipient">Recipient</label>
+                    <Input
+                        id="recipient"
+                        value={inputs.recipient}
+                        onChange={(event) =>
+                            setInputs({
+                                ...inputs,
+                                recipient: event.target.value,
+                            })
+                        }
+                    />
+                </Form.Field>
+                <Button>Create</Button>
+            </Form>
+        </>
+    );
 }
+
+RequestForm.getInitialProps = async ({ query }) => {
+    const { campaignAddress } = query;
+
+    return { campaignAddress };
+};
 
 export default RequestForm;
